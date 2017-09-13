b4d04e66f174bdc071fccd46482a32c3e271f129
Carson.Freeman <Carson.Freeman@MU-C1-08.mullen.pvt.k12.co.us>
2017-09-13 15:02:27 -0600

    initial commit

---
+++ arraylist/src/arraylist/arrayList.java
@@ -0,0 +1,15 @@
+package arraylist;
+
+import java.util.ArrayList;
+
+public class arrayList
+	{
+
+		public static void main(String[] args)
+			{
+				ArrayList<Double> bodyTemps = new ArrayList<Double>();
+				bodyTemps.add();
+
+			}
+
+	}
