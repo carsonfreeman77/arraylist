471bc80e7d4c1cb16498ede41e83c40910bfe7ee
willy <willy@10.0.0.23>
2017-09-13 23:53:05 -0600

    home

--- arraylist/src/arraylist/arrayList.java
+++ arraylist/src/arraylist/arrayList.java
@@ -8,8 +8,40 @@ public class arrayList
 		public static void main(String[] args)
 			{
 				ArrayList<Double> bodyTemps = new ArrayList<Double>();
-				bodyTemps.add();
-
+				bodyTemps.add(95.0);
+				bodyTemps.add(94.3);
+				bodyTemps.add(96.7);
+				bodyTemps.add(96.3);
+				bodyTemps.add(98.8);
+				bodyTemps.add(99.1);
+				bodyTemps.add(103.0);
+				bodyTemps.add(101.0);
+				bodyTemps.add(98.6);
+				bodyTemps.add(95.8);
+				
+				
+				
+		
+			for (int i = 0; i < bodyTemps.size(); i++) 
+			{
+				
+				System.out.println(bodyTemps.get(i) + " degrees Fahrenheit");
 			}
+			System.out.println("People with a fever: ");
+			
+			removingFever(bodyTemps);
+			
+			
+		}
+
+		private static void removingFever(ArrayList<Double> bodyTemps) {
+			bodyTemps.remove(1);
+			bodyTemps.remove(2);
+			bodyTemps.remove(3);
+			bodyTemps.remove(4);
+			bodyTemps.remove(10);
+			System.out.println(bodyTemps);
+			
+		}
 
 	}
